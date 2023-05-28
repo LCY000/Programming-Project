1157f2f64e49c4022fc887d4c8df474642f612ab
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-05-28 21:43:19 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -42,12 +42,12 @@ def callback():
 @webhook_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_id = event.source.user_id
-    message = event.message.text
+    # message = event.message.text
 
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=message))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":

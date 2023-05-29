cee6601719b8d5eeadd192f076064916f56279e3
Evan Lo <monstercontcer1@gmail.com>
2023-05-29 20:33:41 +0800

    新增-新增待辦事項功能:0529-2033

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 from api import ToDo_task
+import datetime
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -8,7 +9,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 )
 
 app = Flask(__name__)
@@ -16,10 +17,71 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('LlcraQZMrH5dj81FA7Cr61wjDwdIAGvxrAohTctu0ukg69/WZVtMyJXVAgMylX7L7HbY1R22i9CqSqqOQ00iRUaqSs2A1Nblbu4iz4fub3xRhKw8JEj7D0mIBCYT9aN8eV1M2BXD1fJxl8s8ny915wdB04t89/1O/w1cDnyilFU=')
 webhook_handler = WebhookHandler('8f948b2d6deda1511f4570128cd231a0')
 
+# 待辦事項列表
+todoList = []
+
+# 將待辦事項加入列表
+def addTodoList(task):
+    todoList.append(task)
+
+# 取得待辦事項清單
+def getTodoList():
+    return todoList
+
+# 處理接收到的訊息事件
+@webhook_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_id = event.source.user_id
+    user_message = event.message.text
+
+    if user_message == '加新的待辦事項':
+        add_items()
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text= user_message))
+    
+# 新增待辦事項
+def add_items(event):
+
+    # 創建一個新的待辦事項
+    new_task = ToDo_task("新的待辦事項內容")
+
+    # 將待辦事項加入列表
+    addTodoList(new_task)
+
+    # 取得待辦事項清單
+    todoList = getTodoList()
+
+    # 建立訊息窗格，條列顯示待辦事項清單
+    message = createTodoListMessage(todoList)
+
+    # 回覆訊息給使用者
+    line_bot_api.reply_message(event.reply_token, message)
+
+def createTodoListMessage(todoList):
+    # 建立Flex Message物件，用於顯示待辦事項清單
+    flex_message = FlexSendMessage(
+        alt_text = "待辦事項清單",
+        contents = {
+            "type" : "bubble",
+            "body" : {
+                "type" : "box",
+                "layout" : "vetical",
+                "contents" : [
+                    {"type" : "text", "text" : "待辦事項清單", "weight" : "bold", "size" : "lg"},
+                    # 根據待辦事項清單建立條列項目
+                    # 可以使用迴圈遍歷待辦事項清單，建立對應的條列項目
+                ]
+            }
+        }
+    )
+    return flex_message
+
 @app.route("/")
 def home():
     return "LINE BOT API Server is running."
-
+ 
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -38,20 +100,5 @@ def callback():
 
     return 'OK'
 
-
-@webhook_handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_id = event.source.user_id
-    message = event.message.text
-
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=message))
-
-
 if __name__ == "__main__":
     app.run()
-
-
-

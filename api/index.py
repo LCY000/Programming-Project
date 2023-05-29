316531a2b2d99b185e0395216ad9a69444db6ac7
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-05-30 02:16:17 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -90,7 +90,7 @@ def handle_add_todo_state(user_id, user_message):
         reply_message = reply_message+'  已結束新增待辦事項。 in add_todo A '+ 'state=' + str(user_state[user_id].value)
     else:
         # 創建一個新的待辦事項
-        new_task = ToDotask(user_message)
+        new_task = ToDotask(text = user_message)
         addTodoList(user_id,new_task)
         reply_message = '已新增待辦事項：{}'.format(user_message)
 

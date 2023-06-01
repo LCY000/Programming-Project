b3d0f31bbeb556837293c599bf0f2ede6e7e536d
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-06-02 02:09:43 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -71,7 +71,7 @@ def handle_normal_state(user_id, user_message, event):
         user_state[user_id] = UserState.ADD_TODO
         reply_message = f'請輸入待辦事項內容。'
         # 關閉圖文選單
-        line_bot_api.unlink_rich_menu_from_user(user_id,rich_menu_id)
+        line_bot_api.unlink_rich_menu_from_user(user_id,rich_menu_id, timeout=5)
         # 還需製作一個取消新增訊息的功能......
 
     elif user_message == '顯示 待辦清單':
@@ -113,7 +113,7 @@ def handle_message(event):
             reply_message,user_todo_list = Function.handle_add_todo_state(user_id, user_message,user_todo_list)
             user_state[user_id] = UserState.NORMAL
             # 開啟圖文選單
-            line_bot_api.link_rich_menu_to_user(user_id,rich_menu_id)
+            line_bot_api.link_rich_menu_to_user(user_id, rich_menu_id, timeout=5)
     
     # 等待新增功能中......
 

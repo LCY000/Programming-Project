f5b1343ae39a88dda7da615bc8bc8c37f8b1059a
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-06-03 15:01:43 +0800

    Update Function.py

--- api/Function.py
+++ api/Function.py
@@ -67,7 +67,6 @@ def handle_del_todo_state(user_id, user_message, user_todo_list):
             reply_message = f"已完成: {user_todo_list[user_id][number-1]['text']}"
             del user_todo_list[user_id][number-1]  # 刪除匹配的待辦事項內容
             
-        
         else:
             reply_message = f'未找到此待辦事項' # 如果沒有找到對應的待辦事項內容，則回傳此訊息
 

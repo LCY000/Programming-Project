ac3aba4d685348e0d126544186cc823e1ade51b4
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-06-03 20:29:03 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -201,7 +201,7 @@ if __name__ == "__main__":
 
     # 建立一個新的執行緒來執行 start_reminder_check() 函數
     reminder_thread = threading.Thread(target=start_reminder_check)
-    reminder_thread.daemon = True  # 將執行緒設置為守護執行緒，以便在主執行緒結束時自動退出
+    reminder_thread.daemon = False  # 將執行緒設置為守護執行緒，以便在主執行緒結束時自動退出
     reminder_thread.start()  # 啟動執行緒
 
     app.run()

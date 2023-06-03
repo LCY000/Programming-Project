85e7521ca8ff53f89e0e78003f1db0a99398c5e2
Evan Lo <monstercontcer1@gmail.com>
2023-06-03 17:55:35 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -154,8 +154,8 @@ def handle_message(event):
 
     if Function.check_reminder_time(reminder_time):
         message = '提醒：您有待辦事項需要處理！'
-        reminder_time_str = reminder_time.strftime("%H:%M:%S")
-        line_bot_api.push_message(user_id, TextSendMessage(text=message),reminder_time_str)
+        # reminder_time_str = reminder_time.strftime("%H:%M:%S")
+        line_bot_api.push_message(user_id, TextSendMessage(text=message))
 
 
     # 輸出回覆訊息 (預防突發意外，保險偵錯)

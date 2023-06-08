d916b9a5088236c3e56323b369a43fadfe4fd91c
Evan Lo <monstercontcer1@gmail.com>
2023-06-08 12:31:41 +0800

    Update Function.py

--- api/Function.py
+++ api/Function.py
@@ -98,7 +98,7 @@ def createTodoListMessage(user_id, user_todo_list, fixed_reminder_times):
         for todo in todoList:
             
             if 'remind_time' in todo:
-                remind_time_text = f"   提醒: {todo['remind_time']}"
+                remind_time_text = f"     提醒: {todo['remind_time']}"
                 item = {
                     "type": "box",
                     "layout": "vertical",
@@ -241,7 +241,7 @@ def setting_state(user_message, user_id, user_todo_list, user_state):
 
     elif user_message == '顯示 說明文件':
 
-        reply_message = '1. 在任何情況下輸入「取消」或「0」\n    即可中斷、跳出當下功能\n\n已回到主選單狀態。'
+        reply_message = '1. 在任何情況下輸入「取消」或「0」即可中斷、跳出當下功能\n\n已回到主選單狀態。'
         user_state[user_id] = index.UserState.NORMAL
     
     elif user_message == '設定特定待辦事項提醒時間':
